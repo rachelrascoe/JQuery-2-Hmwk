abd7a09564452157930ca295dc263eb45b703864
rachelrascoe <rachelrascoe@utexas.edu>
2016-11-29 18:48:41 -0600

    Plain Table

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
 
-$(document).ready(function() {
-  $('#sky').jQlouds();
-});
-
-$('#sky').jQlouds({
-  minClouds: 30, // minimum amount of clouds
-  maxClouds: 50, // maximum amount of clouds
-  wind: true,
-});
